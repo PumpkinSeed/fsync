e84232cd987e77a0d9a0d21cc1b228fcc0896480
PumpkinSeed <qwer.kocka@gmail.com>
2023-03-18 22:05:38 +0100

    Add small fix

--- src/commit.rs
+++ src/commit.rs
@@ -2,13 +2,16 @@ use crate::etc;
 use crate::utils;
 use std::env;
 use uuid::Uuid;
+use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
 
 pub fn action(c: etc::Config) {
     let temp_location = utils::get_temp_location();
     utils::git::clone(c.repository.as_str(), temp_location);
 
     for file in c.files.iter() {
-        utils::file_checksum(file.to_string())
+        let checksum = utils::file_checksum(file.to_string());
+        let encoded = general_purpose::STANDARD.encode(file.to_string());
+        println!("{}: {}", encoded, checksum);
     }
 }
 
@@ -16,6 +19,7 @@ pub fn action(c: etc::Config) {
 mod tests {
     use crate::{commit, etc};
 
+    #[test]
     fn test_action() {
        commit::action(etc::Config{
            repository: String::from("git@github.com:PumpkinSeed/fsync-test.git"),
